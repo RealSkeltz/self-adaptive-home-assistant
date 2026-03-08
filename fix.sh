6f67065acb6c9f8aedbd1865ce0475377e75f18a
Bob Improvement Agent <bob-assistant@improvement-agent.ai>
2026-03-08 23:34:20 +0100

    change to fix

--- fix.sh
+++ fix.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-VENV=$(poetry env info --path)
-sed -i 's/import pkg_resources//' $VENV/lib/python3.12/site-packages/webrtcvad.py
-sed -i 's/__version__ = pkg_resources.get_distribution(.webrtcvad.).version/__version__ = "2.0.10"/' $VENV/lib/python3.12/site-packages/webrtcvad.py
-echo "Fixed webrtcvad!"
+# Detect OS and set path accordingly
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    FILE=/Users/jscheltema/Library/Caches/pypoetry/virtualenvs/home-assistant-NIV9SJTX-py3.12/lib/python3.12/site-packages/webrtcvad.py
+else
+    FILE=/home/realskeltz/.cache/pypoetry/virtualenvs/home-assistant-6T_GrIVA-py3.13/lib/python3.13/site-packages/webrtcvad.py
+fi
+
+echo "Fixing: $FILE"
+sed -i${SED_FLAG} 's/import pkg_resources//' "$FILE"
+sed -i${SED_FLAG} 's/__version__ = pkg_resources.get_distribution(.webrtcvad.).version/__version__ = "2.0.10"/' "$FILE"
+echo "Done!"
